5eb5ba41d31884f564cb4e77754183e5655af976
MatheusGardin <jackmatheus123@gmail.com>
2021-10-02 16:50:18 -0300

    Pagination

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -25,15 +25,31 @@
                             <thead>
                                 <tr>
                                     <th scope="col">SL No</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Email</th>
+                                    <th scope="col">Category Name</th>
+                                    <th scope="col">User</th>
                                     <th scope="col">Created at</th>
                                 </tr>
                             </thead>
                             <tbody>
-
+                                {{-- @php($categories = App\Models\Category::all()) --}}
+                                @foreach($categories as $category)
+                                    <tr>
+                                        <th scope="row"> {{ $categories->firstItem()+$loop->index }} </th>
+                                        <td> {{ $category->category_name }} </td>
+                                        <td> {{ $category->user_id }} </td>
+                                        <td>
+                                            @if($category->created_at === NULL)
+                                                <span class="text-danger"> No Date Set </span>
+                                            @else
+                                                {{ $category->created_at->diffForHumans() }}
+                                                {{-- {{ Carbon\Carbon::parse($category->created_at)->diffForHumans() }} --}}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
+                        {{ $categories->links() }}
                     </div>
                 </div>
                 <div class="col-md-4">
